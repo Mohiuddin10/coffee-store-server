559359c579e2ca15d7356e12d14c671e8b3a4473
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-01-22 10:13:19 +0600

    updated logedin time set

--- index.js
+++ index.js
@@ -95,6 +95,18 @@ async function run() {
         res.send(result);
     })
 
+    app.patch('/users', async (req, res) => {
+        const user = req.body;
+        const filter = { email: user.email }
+        const updatedDoc = {
+            $set: {
+                lastLoggedAt: user.lastLoggedAt
+            }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+    })
+
     app.delete("/users/:id", async (req, res) => {
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
