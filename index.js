512f6705f0ff0eadb900152c857e625f8f03ed2a
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-01-23 22:58:21 +0600

    versel

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 app.use(cors());
 app.use(express.json())
 
